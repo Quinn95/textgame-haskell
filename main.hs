2cc93684aa7155d77c540ba773782579e793bc7e
Quinn Z <quinn95@gmail.com>
2017-01-02 00:58:50 -0600

    Got a very crude implementation of what a StateT gameloop would look like, among some other random things

--- main.hs
+++ main.hs
@@ -1,18 +1,23 @@
+import Control.Monad
+import Control.Monad.State
+
+
 data Direction = N | S | E | W deriving (Show, Eq)
 
 type Position = (Int, Int)
+type Health = Int
 
+-- Items have a flavortext and a function for their action
 data Effect = Effect { flavor :: String
-                     , function :: (IO ()) 
+                     , function :: (IO ()) -- function type will probably change
                      }
-
 instance Show Effect where
-	show = flavor
+    show = flavor
 
 
 
 class Positional a where
-	getPos :: a -> Position
+    getPos :: a -> Position
 
 
 
@@ -20,25 +25,63 @@ data Actor = Actor { friendly :: Bool
                    , posActor :: Position
                    } deriving (Show)
 
+instance Positional Actor where
+    getPos = posActor
 
-instance Positional Item where
-	getPos = posActor
-
-data Item = Item { name :: String
+data Item = Item { nameItem :: String
                  , posItem :: Position
-                 , effect :: Effect
+                 , effectItem :: Effect
                  } deriving (Show)
-
-
 instance Positional Item where
-	getPos = posItem
-
-
-
+    getPos = posItem
 
 
 data Room = Room { description :: String
                    , actors :: [Actor]
-                   , items :: [Item]
-                   , doors :: [Direction]
+                   , itemsRoom :: [Item]
+                   , doors :: [(Direction, Room)]
                    } deriving (Show)
+
+-- going to be part of the State monad
+data Player = Player { healthPlayer :: Health
+                     , posPlayer :: Position 
+                     , itemsPlayer :: [Item]
+                     }
+instance Positional Player where
+    getPos = posPlayer
+
+data GameState = Game { player :: Player
+                      , room :: Room
+                      }
+
+
+
+overlap :: Positional a => a -> a -> Bool
+overlap ob1 ob2 = (getPos ob1) == (getPos ob2)
+
+
+initialGameState = Game { player = Player {healthPlayer = 100, posPlayer = (0, 0), itemsPlayer = []},
+                          room = Room {description = "Big room",
+                                        actors = [],
+                                        itemsRoom = [],
+                                        doors = []
+                                        }
+                        }
+transitionRoom :: Room -> IO ()
+transitionRoom x = do
+    print (description x)
+
+loop :: StateT GameState IO ()
+loop = do
+    st <- get
+    let r = room st
+    return ()
+
+main = do
+    transitionRoom (room initialGameState)
+    f <- runStateT loop initialGameState
+    return ()
+
+
+a = Actor {friendly = False, posActor = (0, 0)}
+b = Actor {friendly = False, posActor = (0, 1)}
